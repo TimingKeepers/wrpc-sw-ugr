a03a152ee286173f406702b9db3940e16c19a022
Miguel Jimenez Lopez <klyone@ugr.es>
2014-05-28 14:20:57 +0200

    wrc_main: Improved memory map help message.

--- wrc_main.c
+++ wrc_main.c
@@ -30,15 +30,16 @@ static void obp_initialize()
 }
 
 static void print_found_devices() {
-	mprintf("MINIC: 0x%x\n",(unsigned int)BASE_MINIC);
-	mprintf("EP: 0x%x\n",(unsigned int)BASE_EP);
-	mprintf("SOFTPLL: 0x%x\n",(unsigned int)BASE_SOFTPLL);
-	mprintf("PPS_GEN: 0x%x\n",(unsigned int)BASE_PPS_GEN);
-	mprintf("SYSCON: 0x%x\n",(unsigned int)BASE_SYSCON);
-	mprintf("UART: 0x%x\n",(unsigned int)BASE_UART);
-	mprintf("ONEWIRE: 0x%x\n",(unsigned int)BASE_ONEWIRE);
-	mprintf("ETHERBONE_CFG: 0x%x\n",(unsigned int)BASE_ETHERBONE_CFG);
-	mprintf("WRPC_RAM: 0x%x\n",(unsigned int)BASE_WRPC_RAM);
+	mprintf("DEVICE: ADDR_1B / ADDR_4B \n\n");
+	mprintf("MINIC: 0x%x / 0x%x \n",(unsigned int)BASE_MINIC, (unsigned int)BASE_MINIC/4);
+	mprintf("EP: 0x%x / 0x%x \n",(unsigned int)BASE_EP, (unsigned int)BASE_EP/4);
+	mprintf("SOFTPLL: 0x%x / 0x%x \n",(unsigned int)BASE_SOFTPLL, (unsigned int)BASE_SOFTPLL/4);
+	mprintf("PPS_GEN: 0x%x / 0x%x \n",(unsigned int)BASE_PPS_GEN, (unsigned int)BASE_PPS_GEN/4);
+	mprintf("SYSCON: 0x%x / 0x%x \n",(unsigned int)BASE_SYSCON, (unsigned int)BASE_SYSCON/4);
+	mprintf("UART: 0x%x / 0x%x \n",(unsigned int)BASE_UART, (unsigned int)BASE_UART/4);
+	mprintf("ONEWIRE: 0x%x / 0x%x \n",(unsigned int)BASE_ONEWIRE, (unsigned int)BASE_ONEWIRE/4);
+	mprintf("ETHERBONE_CFG: 0x%x / 0x%x \n",(unsigned int)BASE_ETHERBONE_CFG, (unsigned int)BASE_ETHERBONE_CFG/4);
+	mprintf("WRPC_RAM: 0x%x / 0x%x \n",(unsigned int)BASE_WRPC_RAM, (unsigned int)BASE_WRPC_RAM/4);
 }
 
 int main(void)
